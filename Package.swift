0bf931e38393dd0987b714327449048e11908547
Zero <zero.griffin@gmail.com>
2018-06-30 03:47:11 +0200

    Remove swift package manager

--- Package.swift
+++
@@ -1,8 +0,0 @@
-// swift-tools-version:4.0
-import PackageDescription
-
-let package:Package = Package(
-    name:"SwiftToShell",
-    products:[.library(name:"SwiftToShell", type:.dynamic, targets:["SwiftToShell"])],
-    targets:[.target(name:"SwiftToShell", dependencies:[])]
-)
